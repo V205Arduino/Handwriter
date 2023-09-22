6f488ad1fab590573fc4bb9ba32ef2cbe2bd8ffc
V205 <138826219+V205Arduino@users.noreply.github.com>
2023-09-22 11:02:59 -0700

    Update Handwriter.js

--- Handwriter.js
+++ Handwriter.js
@@ -1,1 +1,27 @@
+// Handwriter.js for Blot.
+
+const width = 125;
+const height = 125;
+var text = "a23";// better var or const?????
+
+var textLength = text.length;
+
+
+setDocDimensions(width, height);
+
+const testTurtle = createTurtle();
+nsole.log
+for (let i = 0; i < textLength; i++) {
+    testTurtle.forward(i);
+    testTurtle.left(91);
+}
+
+testTurtle.translate(
+  [width/2, height/2], 
+  testTurtle.cc
+);
+
+drawTurtles(
+    testTurtle
+);
 //Placeholder.
